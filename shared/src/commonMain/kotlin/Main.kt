fb10485ee0019ab444b3e950f87001e9bd6ff4cd
i <i@ohyooo.com>
2026-02-02 19:55:21 +0800

    add functions

--- shared/src/commonMain/kotlin/Main.kt
+++ shared/src/commonMain/kotlin/Main.kt
@@ -1,11 +1,21 @@
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
+fun getStatus(): Int = runBlocking {
     val client = HttpClient()
-    val response: HttpResponse = client.get("https://ktor.io/")
-    println(response.status)
+    val response: HttpResponse = client.get("http://ktor.io/")
+    response.status
     client.close()
+    response.status.value
+}
+
+fun getContent(): String = runBlocking {
+    val client = HttpClient()
+    val response: HttpResponse = client.get("http://ktor.io/")
+    response.status
+    client.close()
+    response.bodyAsText()
 }
