306566f75dcc7bba8beb3c3dd0b5f70202de0179
i <i@ohyooo.com>
2026-02-04 21:49:46 +0800

    remove log

--- shared/src/commonMain/kotlin/Main.kt
+++ shared/src/commonMain/kotlin/Main.kt
@@ -13,9 +13,7 @@ fun getStatus(): Int = runBlocking {
         val response: HttpResponse = client.get("http://google.com/")
         response.status.value
     } catch (e: Throwable) {
-        println("aaaaaaaaaa =======1======== aaaaaaaaaa")
         e.printStackTrace()
-        println("aaaaaaaaaa =======2======== aaaaaaaaaa")
         0
     } finally {
         client.close()
@@ -30,9 +28,7 @@ fun getContent(): String = runBlocking {
         val response: HttpResponse = client.get("http://google.com/")
         response.bodyAsText()
     } catch (e: Throwable) {
-        println("aaaaaaaaaa ---------1-------- aaaaaaaaaa")
         e.printStackTrace()
-        println("aaaaaaaaaa ---------2-------- aaaaaaaaaa")
         e.toString()
     } finally {
         client.close()
